f923bda19e37332edc18855edd9203f3ee44eca0
Alex Fuss <alexfuss2@gmail.com>
2020-07-23 12:58:47 -0400

    Added in proper dependencies for the project.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Create constants for node package
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
+
+
 // array of questions for user
 const questions = [
 
